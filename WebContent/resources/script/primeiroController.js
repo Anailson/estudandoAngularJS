9c1637f8bdb17e50e1850310e1d9cdbd1aba98fd
Anailson Ribeiro <arsantos315@gmail.com>
2020-10-01 11:57:16 -0300

    Select

--- WebContent/resources/script/primeiroController.js
+++ WebContent/resources/script/primeiroController.js
@@ -4,37 +4,13 @@ var app = angular.module('loja', [ 'ngRoute', 'ngResource']);
 //var app = angular.module('loja', ['ngRoute']);
 //var app = angular.module('loja', [ 'ngRoute', 'ngResource']);
 
-app.controller('pegarResposta', function($scope, $http) {//INICIO ANGULAR HTTP
-	$scope.pegarResposta = function () {
-		 $http.get("pegarResposta").then(function(response){
-			 document.getElementById("resposta").value =""+response.data;
-		 });
-	};
-  
+
+app.controller('namesController', function($scope) {
+  $scope.names = ["Email", "Anailson", "Santos"];
 });
 
-app.controller('pegarResposta2', function($scope, $http) {
-	$scope.pegarResposta2 = function () {
-		 $http.get("pegarResposta").then(function(response){
-			 document.getElementById("resposta2").value =""+response.data;
-			 document.getElementById("resposta3").value =""+response.status;
-			 document.getElementById("resposta4").value =""+response.statusText;
-		 });
-	};
-  
-});
 
-app.controller('pegarRespostaErro', function($scope, $http) {
-	$scope.pegarRespostaErro = function () {
-		 $http.get("pegarRespostaErro")
-		 .then(function(response){//RESPOSTA BEM SUCEDIDA
-			 document.getElementById("respostaerro").value =""+response.statusText;
-		 }, function(response){//LIDA COM ERROS
-			document.getElementById("respostaerro").value =""+response.statusText;
-		 } );
-	};
-  
-});
+
 //UTILIZANDO NAS TABELAS
 app.controller('pegarRespostaJson', function ($scope, $http) {
 	$http.get("pegarRespostaJson").then(function (response){
